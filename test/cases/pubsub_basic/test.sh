1802e091c48a362cc4fba085c3d9e14472550fb4
gaut <gaut@jpl.nasa.gov>
2021-06-19 18:37:11 -0700

    Updated test

--- test/cases/pubsub_basic/test.sh
+++ test/cases/pubsub_basic/test.sh
@@ -1,11 +1,17 @@
-port=9253
+port=$(../../common/find-free-port)
+printf "Using port $port\n"
+
+rm -rf artifacts
 mkdir -p artifacts
 
+printf "Starting server\n"
 SPOKEPORT=$port spoke &
 PID=$!
 sleep 0.2
+printf "Starting echo client\n"
 SPOKEPORT=$port spoke-echo foo | tee artifacts/output.txt &
 sleep 0.2
+printf "Publishing message\n"
 SPOKEPORT=$port spoke-publish foo 5 &
 sleep 0.2
 
