2dd125905957b1cc89a3ffb723cc2f44ac9bdc8c
samflam <me@samflam.com>
2021-08-25 23:37:10 -0700

    Forgot to add module

---
+++ src/spoke/pubsub/pack.py
@@ -0,0 +1,52 @@
+import spoke
+
+
+class Message:
+    def __init__(self, channel, body, **head):
+        self.head = head.copy()
+        self.channel = channel
+        self.body = body
+
+    @property
+    def channel(self):
+        return self.head["channel"]
+
+    @channel.setter
+    def channel(self, value):
+        if not isinstance(value, str):
+            raise ValueError("channel is not a string")
+        self.head["channel"] = spoke.pubsub.route.canonical(value)
+
+
+class MessagePacker(spoke.conn.abc.AbstractPacker):
+    def __init__(self):
+        self.__json_packer = spoke.conn.pack.JsonPacker()
+
+    def pack(self, msg):
+        json_msg = {
+            "head": msg.head,
+            "body": msg.body,
+        }
+        return self.__json_packer.pack(json_msg)
+
+    def unpack(self, data):
+        self.__json_packer.unpack(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        json_msg = next(self.__json_packer)
+        if not isinstance(json_msg, dict):
+            raise ValueError("json_msg is not a dict")
+        head = json_msg.get("head", None)
+        if not isinstance(head, dict):
+            raise ValueError("head is not preset or is not a dict")
+        channel = head.get("channel", None)
+        if not isinstance(channel, str):
+            raise ValueError("channel is not preset or is not a string")
+        body = json_msg.get("body", None)
+        return Message(body=body, **head)
+
+    def reset(self):
+        self.__json_packer.reset()
